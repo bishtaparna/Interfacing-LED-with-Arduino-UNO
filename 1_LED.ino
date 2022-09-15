f444557ab4b977c41fddf60e3735c8f362894b9c
Aparna Bisht <bishta411@gmail.com>
2022-09-15 19:47:39 +0530

    Update 1_LED.ino

--- 1_LED.ino
+++ 1_LED.ino
@@ -1,20 +1,23 @@
 /*To blink the LED*/
 
+ // configure pin2 as output pin for LED
+const int LED = 2;
+
 void setup() 
 {
-  // configure pin2 as output pin
-  pinMode(2, OUTPUT); 
+ 
+  pinMode(LED, OUTPUT);   //Here, the mode of the pin in which LED is connected and will give output.
   
 }
 
 void loop() 
 {
   // Turn ON the LED
-  digitalWrite(2, HIGH);
+  digitalWrite(LED, HIGH);
   delay(1000);
 
   // Turn OFF the LED
-  digitalWrite(2, LOW);
+  digitalWrite(LED, LOW);
   delay(1000);  
 
 }
